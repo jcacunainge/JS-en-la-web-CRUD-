af591a43fc0c045d542b27a96b32b2dbef1fe5dc
Jhan Carlos Acuña Ordoñez <jcacunainge@gmail.com>
2022-11-05 15:15:11 -0500

    metodo post

--- service/client-service.js
+++ service/client-service.js
@@ -1,1 +1,18 @@
-console.log("SI sirvio")
+const listaCliente = () => fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
+
+
+const crearCliente = (nombre, email)=> {
+    return fetch("http://localhost:3000/perfil", {
+        method: "POST",
+        headers: {"content-type":"aplication/json"},
+        body: JSON.stringify({nombre, email, id:uuid.v4()}),
+    });
+};
+
+export const clientService = {
+    listaCliente,
+    crearCliente,
+}
+
+
+
